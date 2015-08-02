a56c4222df48e9fd6c9619591f7c0db96fa20b8a
osxi <osxi@users.noreply.github.com>
2015-08-02 13:09:00 -0500

    initial source and tests

---
+++ game_of_life.el
@@ -0,0 +1,43 @@
+;;; Package --- Game of life in elisp
+
+;;; Commentary:
+;;;   TODO
+
+;;; Code:
+
+(defun should-live (neighbor-count)
+  "Given NEIGHBOR-COUNT, determine if cell should live or die."
+  (cond ((< neighbor-count 2) nil)
+        ((eq neighbor-count 2) t)
+        ((eq neighbor-count 3) t)
+        ((> neighbor-count 4) nil)
+        (t nil)))
+
+(defun count-neighbors (board x y)
+  "Count live neighbor cells for BOARD given relative X and Y."
+  (let ((count 0))
+    (if (lookup-x-y board (- x 1) (- y 1))
+        (setq count (1+ count)))
+    (if (lookup-x-y board x (- y 1))
+        (setq count (1+ count)))
+    (if (lookup-x-y board (1+ x) (- y 1))
+        (setq count (1+ count)))
+    (if (lookup-x-y board (- x 1) y)
+        (setq count (1+ count)))
+    (if (lookup-x-y board (1+ x) y)
+        (setq count (1+ count)))
+    (if (lookup-x-y board (- x 1) (1+ y))
+        (setq count (1+ count)))
+    (if (lookup-x-y board x (1+ y))
+        (setq count (1+ count)))
+    (if (lookup-x-y board (1+ x) (1+ y))
+        (setq count (1+ count)))
+    count))
+
+(defun lookup-x-y (board x y)
+  "Convert BOARD coordinates X and Y into a 2-D vector accessor."
+  (elt (elt board y) x))
+
+(provide 'game_of_life)
+
+;;; game_of_life.el ends here
---
+++ game_of_life_test.el
@@ -0,0 +1,34 @@
+;;; Package --- Tests for game of life in elisp
+
+;;; Commentary:
+;;;   TODO
+
+;;; Code:
+
+(load-file "game_of_life.el")
+
+(ert-deftest game-of-life-core-test ()
+  "Tests game of life core survival rules."
+    (should (equal (should-live 1) nil))
+    (should (equal (should-live 2) t))
+    (should (equal (should-live 3) t))
+    (should (equal (should-live 4) nil)))
+
+(ert-deftest game-of-life-count-neighbors-test ()
+  "Tests count-neighbors function."
+  (let ((board '[[nil nil nil]
+                 [nil t   nil]
+                 [nil nil nil]]))
+    (should (equal (count-neighbors board 1 1) 0))
+    (let ((board-populated '[[t   nil nil]
+                           [nil t   nil]
+                           [nil nil nil]]))
+      (should (equal (count-neighbors board-populated 1 1) 1)))
+    (let ((board-crowded '[[t t t]
+                           [t t t]
+                           [t t t]]))
+      (should (equal (count-neighbors board-crowded 1 1) 8)))))
+
+(provide 'game_of_life_test)
+
+;;; game_of_life_test.el ends here
